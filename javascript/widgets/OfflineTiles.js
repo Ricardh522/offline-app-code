deb971e110ee184284e7d048fab2802e3b2a41a5
Richardh522 <richardh522@gmail.com>
2015-07-13 19:43:39 +0000

    FinalDemo for commit 2

--- javascript/widgets/OfflineTiles.js
+++ javascript/widgets/OfflineTiles.js
@@ -5,7 +5,8 @@ define(["dojo/_base/declare", "dojo/parser", "dojo/ready",  "dojo/on",
 
         startup: function() {
             var tileLayer = O.esri.Tiles.OfflineTileEnablerLayer(
-                "http://52.0.46.248:6080/arcgis/rest/services/RSW/RSW_Airfield_TS/MapServer",
+                // "http://52.0.46.248:6080/arcgis/rest/services/RSW/RSW_Airfield_TS/MapServer",
+                "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer",
                 function (evt) {
                     console.log("Offline tile lib enabled. App is: " + Offline.state);
                 },_isOnline);
