a3f481e4bd2724311497b2f9d72ba7836ac5c0f8
Richardh522 <richardh522@gmail.com>
2015-07-20 22:18:08 +0000

    End of Day Monday

--- javascript/widgets/OfflineMap.js
+++ javascript/widgets/OfflineMap.js
@@ -4,33 +4,37 @@ define(["dojo/_base/declare", "dojo/parser", "esri/geometry/Polygon", "dojo/read
 
      return declare("OfflineMap", [_WidgetBase], {   
     
-     
-        startup: function() {
-           console.log("OfflineMapStartup Function fired");
-        },
-
-         initEvents: function() {
-                  
-                    var map = offlineWidget.map;
-                    map.on("zoom-end",function(evt) {
-                        _currentExtent = evt.extent;
-                        offlineWidget.updateLocalStorage();
-                        Offline.check();
-                    });
-
-                    map.on("pan-end",function(evt) {
-                        _currentExtent = evt.extent;
-                        offlineWidget.updateLocalStorage();
-                        Offline.check();
-                    });
-                    debouncer.setOrientationListener(250,function(){
-                        console.log("orientation"); orientationChange = true;
-                    });
-
-                    document.body.addEventListener('touchmove', function(event) {
-                      event.preventDefault();
-                    }, false); 
-                }
-            });
-    });
  
+    startup: function() {
+       console.log("OfflineMapStartup Function fired");
+    },
+
+     initEvents: function() {
+              
+            var map = offlineWidget.map;
+            map.on("zoom-end",function(evt) {
+                _currentExtent = evt.extent;
+                offlineWidget.updateLocalStorage();
+                Offline.check();
+            });
+
+            map.on("pan-end",function(evt) {
+                _currentExtent = evt.extent;
+                offlineWidget.updateLocalStorage();
+                Offline.check();
+            });
+            debouncer.setOrientationListener(250,function(){
+                console.log("orientation"); orientationChange = true;
+            });
+
+
+            document.addEventListener('touchmove', function(event) {
+              if (!$(event.target).parents().hasClass("touch-moveable"))
+                  {
+                    event.preventDefault();
+                    event.stopPropogation();
+                }
+            } , false); 
+        }
+    });
+});
